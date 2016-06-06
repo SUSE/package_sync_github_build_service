19699ee692c54eee0ca671972875cf7f4651d306
Christian Brauner <cbrauner@suse.com>
2016-06-06 12:08:41 +0200

    Extract version and add "~def" suffix
    Signed-off-by: Christian Brauner <cbrauner@suse.com>

--- packages/docker/docker_sync_obs.sh
+++ packages/docker/docker_sync_obs.sh
@@ -5,7 +5,12 @@ cd $(dirname $0)
 . ../../package_sync_functions.sh
 
 docker_git_version() {
-  echo $(git tag | grep -E '^v[0-9]+(\.[0-9]+)*$' | sort -Vr | head -n1 | sed "s/v//")
+	cat VERSION | grep -q ".0-dev"
+	if [ $? == 0 ]; then
+		echo $(cat VERSION | sed "s/\.0-dev/~def/")
+	else
+		echo $(cat VERSION | sed "s/-dev/~def/")
+	fi
 }
 
 package_sync_build_service https://api.opensuse.org Virtualization:containers:experimental docker https://github.com/SUSE/docker.mirror.git docker_git_version
