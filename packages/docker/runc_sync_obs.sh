cad352279dc20cd71b4f2c67c2993dcfbe404dbd
Thomas Boerger <thomas@webhippie.de>
2016-06-13 10:42:25 +0200

    Fixed versioning for runc
    Since the version of runc includes a -rcX I'm replacing it not also with
    ~dev.
    
    Signed-off-by: Thomas Boerger <thomas@webhippie.de>

--- packages/docker/runc_sync_obs.sh
+++ packages/docker/runc_sync_obs.sh
@@ -5,7 +5,11 @@ cd $(dirname $0)
 . ../../package_sync_functions.sh
 
 runc_git_version() {
-	echo $(cat VERSION)
+	if grep -q -- '.0-rc[0-9]' VERSION; then
+		echo $(cat VERSION | sed "s/\.0-rc[0-9]/~dev/")
+	else
+		echo $(cat VERSION | sed "s/-rc[0-9]/~dev/")
+	fi
 }
 
 package_sync_build_service https://api.opensuse.org Virtualization:containers:experimental runc https://github.com/opencontainers/runc.git runc_git_version
